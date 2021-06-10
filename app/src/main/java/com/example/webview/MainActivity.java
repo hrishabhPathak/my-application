8f908ffc26b0ebc5d938bab3132a872c6ab7c0d5
Vedas IT Solutions <hrishabhpathak006@gmail.com>
2021-06-10 16:46:04 +0530

    this is web view implementation from web group this great and easy method

---
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.wb);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://vedasitsolutions.com/");
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (webView.canGoBack()){
+            webView.goBack();
+            }
+        else {
+            super.onBackPressed();
+        }
+    }
+}
